8d7ee69b552ad47df842164c7f681dce3ff43b35
Tobias Jungel <tobias.jungel@gmail.com>
2023-01-21 20:57:40 +0100

    disable wireguard interface in trap

--- run.sh
+++ run.sh
@@ -3,13 +3,16 @@
 set -e
 
 WG_INTERFACE=${WG_INTERFACE:-wgc0}
+WG_UP=false
 
 _exit_trap() {
     local exit_code=$?
     echo "Stopping due to exit code ${exit_code}"
+    ${WG_UP} && sudo wg-quick down "${WG_INTERFACE}"
 }
 trap _exit_trap EXIT
 
 sudo wg-quick up ${WG_INTERFACE}
+WG_UP=true
 sleep infinity &
 wait $!
