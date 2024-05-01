488ca0afb759557d276e4d3344cfb1cbab88aef4
Tobias Jungel <tobias.jungel@gmail.com>
2024-05-01 11:27:11 +0200

    masquerading and other improvements
    * enable masquerading on eth+ by default
    * pin apk versions
    * make shellcheck happy

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,17 @@ FROM docker.io/alpine:3.19.1@sha256:c5b1261d6d3e43071626931fc004f70149baeba2c8ec
 ARG WG_INTERFACE=wgc0
 ENV WG_INTERFACE=${WG_INTERFACE}
 
+# renovate: datasource=repology depName=alpine_3_19/iptables
+ENV IPTABLES_VERSION="1.8.10-r3"
+# renovate: datasource=repology depName=alpine_3_19/sudo
+ENV SUDO_VERSION="1.9.15_p2-r0"
+# renovate: datasource=repology depName=alpine_3_19/wireguard-tools-wg-quick
+ENV WIREGUARD_TOOLS_WG_QUICK_VERSION="1.0.20210914-r3"
+
 RUN apk --no-cache add \
-  sudo \
-  wireguard-tools-wg-quick
+  iptables=${IPTABLES_VERSION} \
+  sudo=${SUDO_VERSION} \
+  wireguard-tools-wg-quick=${WIREGUARD_TOOLS_WG_QUICK_VERSION}
 
 COPY wg-sudoers /etc/sudoers.d/wg-sudoers
 
--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 WG_INTERFACE=${WG_INTERFACE:-wgc0}
 WG_UP=false
+MASQUERADE_INTERFACE="eth+"
+MASQUERADE_ENABLED=${MASQUERADE_ENABLED:-true}
 
 _exit_trap() {
     local exit_code=$?
     echo "Stopping due to exit code ${exit_code}"
-    ${WG_UP} && sudo wg-quick down "${WG_INTERFACE}"
+    if [ "${WG_UP}" = true ]; then
+      sudo wg-quick down "${WG_INTERFACE}"
+      [ "$MASQUERADE_ENABLED" = true ] && sudo /sbin/iptables -t nat -D POSTROUTING -o "$MASQUERADE_INTERFACE" -j MASQUERADE
+    fi
 }
 trap _exit_trap EXIT
 
-sudo wg-quick up ${WG_INTERFACE}
+sudo wg-quick up "${WG_INTERFACE}"
 WG_UP=true
+[ "$MASQUERADE_ENABLED" = true ] && sudo /sbin/iptables -t nat -A POSTROUTING -o "$MASQUERADE_INTERFACE" -j MASQUERADE
 sleep infinity &
 wait $!
